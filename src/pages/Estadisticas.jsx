5230fa6e54d8d0ecaf24b1b28fb57cd7302eb56c
Mauricio Damian Gomez Galdamez <L21270287@tuxtla.tecnm.mx>
2026-04-03 23:24:20 -0600

    fix: recaudado useMemo

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -41,15 +41,9 @@ const Estadisticas = () => {
   }, 0);
 
   // 💵 RECAUDADO (pagos reales)
-  const totalRecaudado = useMemo(() => {
-    return ordenes.reduce((acc, o) => {
-      const totalOrden = (o.productos || []).reduce((sum, item) => {
-        return sum + Number(item.abono ?? item.total ?? 0);
-      }, 0);
-  
-      return acc + totalOrden;
-    }, 0);
-  }, [ordenes]);
+  const totalRecaudado = ordenes.reduce((acc, o) => {
+    return acc + Number(o.total || 0);
+  }, 0);
 
   // 💸 LO QUE FALTA
   const restanteReal = deudaTotal;
